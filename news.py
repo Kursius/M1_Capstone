aa5f74f8458c1e1676e0d02ccfa06f0e31ca3a7b
Kursius <martynaskursevicius26@gmail.com>
2023-09-09 16:37:49 +0300

    Additional functions

--- news.py
+++ news.py
@@ -3,15 +3,22 @@
 import json
 from dataclasses import dataclass
 import re
+import ssl
+import smtplib
+import requests
+from email.message import EmailMessage
 
 
 # class to store user data(username, email, password)
 @dataclass
 class User:
-    def __init__(self, username=None, email=None, password=None):
+    def __init__(self, username=None, email=None, password=None, news_categories=None, weather_locations=None):
         self.username = username
         self.email = email
         self.password = password
+        self.news_categories = news_categories
+        self.weather_locations = weather_locations
+
 
     def setUsername(self, username):
         # check if username is already taken by reading .json file with username as filename
@@ -46,6 +53,9 @@ class User:
                 "Invalid password."
             )
             return False
+    
+    def getEmail(self):
+        return self.email
 
 
 # using User class to create a new user
@@ -99,10 +109,10 @@ def login():
                 print("Login successful!")
                 loggedIn()
             else:
-                print("Incorrect password. Please try again.")
+                print("Incorrect password. Please try again or reset the password.")
     #if username is not found, user is asked to try again
     except FileNotFoundError:
-        print("User not found. Please try again or register.")
+        print("User not found. Please try again, or register.")
 
 def loggedIn():
     while True:
@@ -118,16 +128,52 @@ def loggedIn():
                 continue
 
 def loggedInMenu():
-    print("1. Search for news")
-    print("2. Exit")
+    print("1. Search for weather")
+    print("2. Change password")
+    print("3. Exit")
     choice = input("Enter your choice: ")
     return choice
+
+#!!!create class for email message
+
+def sendEmail(user, message):
+    email_sender = "mkurseturing@gmail.com"
+    email_pass = "kbjjtmnebcvihioc"
+    email_receiver = user.getEmail()
+
+    em = EmailMessage()
+    em["From"] = email_sender
+    em["To"] = email_receiver
+    em["Subject"] = "Your information"
+    em.set_content(message)
+    context = ssl.create_default_context()
+
+    # with the generated email message, login, authenticate and send email message
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(email_sender, email_pass)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+#function to ask user for his username, check if user exists, if so, send email with link to reset password
+def resetPassword():
+    username = input("Enter your username: ")
+    try:
+        with open(username + ".json", "r") as file:
+            data = json.load(file)
+            print("Password reset email sent to " + data["email"])
+
+    except FileNotFoundError:
+        print("User not found. Please try again, or register.")
+
+#function to send email with 6 dgit code to user, user enters code, if code is correct, user can reset password
+def sendPasswordCode():
+    pass
 
 # menu function to manage options to user (login, register, exit)
 def menu():
     print("1. Login")
     print("2. Register")
-    print("3. Exit")
+    print("3. Reset password")
+    print("4. Exit")
     choice = input("Enter your choice: ")
     return choice
 
@@ -142,6 +188,8 @@ def main():
             case "2":
                 createUser()
             case "3":
+                print("Reset password")
+            case "4":
                 print("Goodbye!")
                 exit()
             case _:
@@ -150,3 +198,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+print("\033[31mThis is red font.\033[0m")
+print("\033[32mThis is green font.\033[0m")
+print("\033[33mThis is yellow font.\033[0m")
+print("\033[34mThis is blue font.\033[0m")
+"""
